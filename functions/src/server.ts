961d8fbadef31c90ce1c5e25ac4a8392820b64df
Martin Sahlen <martin8900@gmail.com>
2022-02-28 23:21:00 +0200

    Add some work for Halvor

--- functions/src/server.ts
+++ functions/src/server.ts
@@ -1,7 +1,18 @@
 import * as express from 'express';
+import axios from 'axios';
+
+//const cache: { [name: string]: string } = {}
 
 const app = express();
-app.get('/', (_, response) => response.send(process.env.NODE_ENV === 'production' ? 'prod' : 'dev'))
+app.get('*', async (req, res) => {
+    console.log(req.path)
+     //@ts-ignore
+    res.header("Access-Control-Allow-Origin", "*")
+    const url = req.path.substring(1)
+    const result = await axios.get(url, {responseType: 'stream'})
+    result.data.pipe(res)
+})
 
 export const server = app;
 
+app.listen(3001)
